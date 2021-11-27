1bff99f82984af55875d3bcc5c5ae7e6ef544ce6
Randiex <randexkane@gmail.com>
2021-11-27 22:02:25 +0530

    UUPP

--- plugins/Play.js
+++ plugins/Play.js
@@ -9,9 +9,9 @@ const Axios = require('axios')
 
 Asena.addCommand({pattern: 'song ?(.*)', fromMe: false, desc: 'play song' , dontAddCommandList: true }, async (message, match) => {
 	
-	await message.client.sendMessage(message.jid, '*wait....*' , MessageType.text, { quoted: message.data });
+	await message.client.sendMessage(message.jid, '*Im Searching your song  👸🏼 ➾ 🔎*' , MessageType.text, { quoted: message.data });
 	
-	const {data} = await axios(`https://zenzapi.xyz/api/play/playmp3?query=${match[1]}&apikey=whitedevil-terrorboy`)
+	const {data} = await axios(`https://zenzapi.xyz/api/downloader/ytmp3?query=${match[1]}&apikey=5d5d06e44d62`)
 	
         const { status, result } = data
 	
@@ -21,7 +21,10 @@ Asena.addCommand({pattern: 'song ?(.*)', fromMe: false, desc: 'play song' , dont
 	
         await message.client.sendMessage(message.jid, LOAD_ING , MessageType.text, { quoted: message.data });
         let msg = '```'
-        msg +=  `TITLE :${result.title}\n\n`        
+        msg +=  `TITLE :${result.title}\n\n`
+        msg +=  `CHNNEL :${result.channel}\n\n`
+        msg +=  `PUBLISHED :${result.published}\n\n` 
+        msg +=  `VIEWS :${result.views}\n\n`       
         msg += '```'
          return await message.client.sendMessage(message.jid,Buffer.from(img.data), MessageType.image, {mimetype: Mimetype.jpg , caption: msg })
         });
